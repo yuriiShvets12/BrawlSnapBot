c9807ae2158282c86a3dcfb7210bfd87647e34de
Bak6767 <us030029@gmail.com>
2024-03-29 17:37:18 +0100

    Была создана функция для создания кнопок при нажатии на кнопку Изменить свой Brawl Stars ID.

--- battons/battons.py
+++ battons/battons.py
@@ -1,6 +1,5 @@
 from aiogram.types import Message, InlineKeyboardMarkup, CallbackQuery, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from Commands.user_cmd import send_profile_photo
 from aiogram import types, Router, F, Bot
 from dotenv import load_dotenv
 from database import bd as db
@@ -19,20 +18,6 @@ async def communication() -> InlineKeyboardMarkup:
     builder.row(InlineKeyboardButton(text = "Изменить Brawl Stars id", callback_data = "change"))
     return builder.as_markup()
 
-
-async def photo(user_id) -> InlineKeyboardMarkup:
-    builder = InlineKeyboardBuilder()
-    for i in range(1, 4):
-        profile_id = str(await getattr(db, f'get_brawl_id_{i}')(user_id))
-        if profile_id in ["False", "None"]:
-            profile_id = "+"
-            callback_data = f"add_brawl_id_{i}"
-        else:
-            callback_data = f"brawl_id_{i}"
-        builder.row(InlineKeyboardButton(text=profile_id, callback_data=callback_data))
-    return builder.as_markup()
-
-
 async def brawl_stars_name(user_id) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     for i in range(1, 4):
@@ -43,6 +28,17 @@ async def brawl_stars_name(user_id) -> InlineKeyboardMarkup:
         else:
             callback_data = f"brawl_id_{i}"
         builder.row(InlineKeyboardButton(text=profile_name, callback_data=callback_data))
+    builder.row(InlineKeyboardButton(text="◀️Назад", callback_data="back"))
+    return builder.as_markup()
+
+async def change_brawl_stars_name(user_id) -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    for i in range(1, 4):
+        profile_name = str(await getattr(db, f'get_brawl_name_{i}')(user_id))
+        if profile_name not in ["False", "None"]:
+            callback_data = f"new_id_{i}"
+            builder.row(InlineKeyboardButton(text=profile_name, callback_data=callback_data))
+    builder.row(InlineKeyboardButton(text="◀️Назад", callback_data="back"))
     return builder.as_markup()
 
 
