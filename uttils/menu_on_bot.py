b0e184fd6d0ab096425cc62843f34adfe51c63a1
Bak6767 <us030029@gmail.com>
2024-04-14 12:16:45 +0200

    В файле menu_on_bot.py была убрана команда /language поскольку этой команды уже нету

--- uttils/menu_on_bot.py
+++ uttils/menu_on_bot.py
@@ -5,20 +5,20 @@ from aiogram.types import BotCommand, BotCommandScopeDefault
 async def set_start_menu(bot: Bot):
     commands = [
         BotCommand(
-            command='/start',
-            description='Запускаем бота',
+            command = "/start",
+            description = "Let's launch the bot",
         ),
         BotCommand(
-            command='/brawl_stars',
-            description='Взаимодействие с твоим Brawl Stars ID',
+            command = "/brawl_stars",
+            description = "Interacting with your Brawl Stars ID",
         ),
         BotCommand(
-            command='/donate',
-            description='Отправить донат проекту',
+            command = "/donate",
+            description = "Send a donation to the project",
         ),
         BotCommand(
-            command='/help',
-            description='Помощь в роботе с ботом',
-        )
+            command = "/help",
+            description = "Help with working with the bot",
+        ),
     ]
     await bot.set_my_commands(commands, BotCommandScopeDefault())
